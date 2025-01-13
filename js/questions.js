5a6a9d96fa8a6bb009f4cf9e64354f197d403ed1
Natalie Gillam <ngillam@hotmail.co.uk>
2025-01-13 23:45:41 +0000

    first commit

---
+++ js/questions.js
@@ -0,0 +1,16 @@
+const questions = [
+  {
+    id: 0,
+    q: "onus",
+    definition: "A responsibility or duty that someone is expected to take on.",
+    options: ["yes", "no"],
+    a: 0,
+  },
+  {
+    id: 1,
+    q: "onus",
+    definition: "A responsibility or duty that someone is expected to take on.",
+    options: ["yes", "no"],
+    a: 0,
+  },
+]
