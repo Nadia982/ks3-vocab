4c114e6fbc16edaf11a23c48f429cfb19c804eaf
Natalie Gillam <ngillam@hotmail.co.uk>
2025-01-18 23:34:17 +0000

    added new words

--- js/questions.js
+++ js/questions.js
@@ -11,9 +11,50 @@ const questions = [
     id: 1,
     q: "gnawing",
     // pronunciation: "",
-    definition: "Biting or chewing something repeatedly, often to wear it down or break it apart. Can also mean a constant, annoying pain or feeling, like something biting or chewing at you.",
-    example: "The gnawing hunger in my stomach made it difficult to concentrate on anything else.",
+    definition:
+      "Biting or chewing something repeatedly, often to wear it down or break it apart. Can also mean a constant, annoying pain or feeling, like something biting or chewing at you.",
+    example:
+      "The gnawing hunger in my stomach made it difficult to concentrate on anything else.",
     options: ["yes", "no"],
     a: 0,
   },
-]
+  {
+    id: 2,
+    q: "rind",
+    example: "He grated the lemon rind and added it to the cake mixture.",
+    definition:
+      "the hard outer layer of cheese, or the tough outer skin of fruits like lemons or oranges.",
+    options: ["yes", "no"],
+    a: 0,
+  },
+  {
+    id: 3,
+    q: "defiantly",
+    definition:
+      "doing something in a bold, rebellious way to show you’re not willing to obey or agree.",
+    example:
+      "She refused to tidy her room, crossing her arms and frowning defiantly",
+    options: ["yes", "no"],
+    a: 0,
+  },
+  {
+    id: 4,
+    q: "senile",
+    example:
+      "The elderly man was becoming somewhat senile, often forgetting where he left his keys.",
+    definition:
+      "Someone who has lost the ability to think clearly or remember things, usually because of old age.",
+    options: ["yes", "no"],
+    a: 0,
+  },
+  // template
+  // {
+  //   id: 2,
+  //   q: "",
+  //   definition: "",
+  //   example: "",
+
+  //   options: ["yes", "no"],
+  //   a: 0,
+  // },
+];
